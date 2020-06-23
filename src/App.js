5bd20e32d4025894c259623d6304d08f36f586c0
Fernando A M Leite <fernandomottaptc@gmail.com>
2020-06-22 22:21:37 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("/repositories").then((response) => {
+      console.log(response);
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const responseData = await api.post("/repositories", {
+        "title": `gostack-${Date.now()}`,
+        "url": "https://github.com/Fernandoaml/gostack-template-conceitos-nodejs",
+        "techs": ["Node.js", "JavaScript"]
+    });
+    const repository = responseData.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const responseData = await api.delete(`/repositories/${id}`);
+    console.log(responseData);
+
+    const repository = repositories.filter((repository) => repository.id !== id)
+    // console.log(repositories)
+    // console.log(repository)
+    setRepositories(repository);
+    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+            </li>
+          );
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
